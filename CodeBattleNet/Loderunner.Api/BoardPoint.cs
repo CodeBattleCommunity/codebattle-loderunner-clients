a2063232faebccca02b822af371395478809dca1
Sergey Voroshilin <medulla_261@mail.ru>
2021-03-18 00:07:26 +0300

    Fixes BoardPoint struct: equality comparison, GetHashCode

--- CodeBattleNet/Loderunner.Api/BoardPoint.cs
+++ CodeBattleNet/Loderunner.Api/BoardPoint.cs
@@ -19,92 +19,92 @@
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-﻿using System;
 
 namespace Loderunner.Api
 {
-    public struct BoardPoint
-    {
-        public readonly int X;
-        public readonly int Y;
+	public struct BoardPoint
+	{
+		public readonly int X;
+		public readonly int Y;
 
-        public BoardPoint(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
+		public BoardPoint(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
 
-        /// <summary>
-        /// Checks is current point on board or out of range.
-        /// </summary>
-        /// <param name="boardSize">Board size to compare</param>
-        public bool IsOutOfBoard(int boardSize)
-        {
-            return X >= boardSize || Y >= boardSize || X < 0 || Y < 0;
-        }
+		/// <summary>
+		/// Checks is current point on board or out of range.
+		/// </summary>
+		/// <param name="boardSize">Board size to compare</param>
+		public bool IsOutOfBoard(int boardSize)
+		{
+			return X >= boardSize || Y >= boardSize || X < 0 || Y < 0;
+		}
 
-        /// <summary>
-        /// Returns new BoardPoint object shifted left to "delta" points
-        /// </summary>
-        public BoardPoint ShiftLeft(int delta = 1)
-        {
-            return new BoardPoint(X - delta, Y);
-        }
+		/// <summary>
+		/// Returns new BoardPoint object shifted left to "delta" points
+		/// </summary>
+		public BoardPoint ShiftLeft(int delta = 1)
+		{
+			return new BoardPoint(X - delta, Y);
+		}
 
-        /// <summary>
-        /// Returns new BoardPoint object shifted right to "delta" points
-        /// </summary>
-        public BoardPoint ShiftRight(int delta = 1)
-        {
-            return new BoardPoint(X + delta, Y);
-        }
+		/// <summary>
+		/// Returns new BoardPoint object shifted right to "delta" points
+		/// </summary>
+		public BoardPoint ShiftRight(int delta = 1)
+		{
+			return new BoardPoint(X + delta, Y);
+		}
 
-        /// <summary>
-        /// Returns new BoardPoint object shifted top "delta" points
-        /// </summary>
-        public BoardPoint ShiftTop(int delta = 1)
-        {
-            return new BoardPoint(X, Y - delta);
-        }
+		/// <summary>
+		/// Returns new BoardPoint object shifted top "delta" points
+		/// </summary>
+		public BoardPoint ShiftTop(int delta = 1)
+		{
+			return new BoardPoint(X, Y - delta);
+		}
 
-        /// <summary>
-        /// Returns new BoardPoint object shifted bottom "delta" points
-        /// </summary>
-        public BoardPoint ShiftBottom(int delta = 1)
-        {
-            return new BoardPoint(X, Y + delta);
-            
-        }
+		/// <summary>
+		/// Returns new BoardPoint object shifted bottom "delta" points
+		/// </summary>
+		public BoardPoint ShiftBottom(int delta = 1)
+		{
+			return new BoardPoint(X, Y + delta);
+		}
 
-        public static bool operator ==(BoardPoint p1, BoardPoint p2)
-        {
-            if (ReferenceEquals(p1, p2))
-                return true;
+		public static bool operator ==(BoardPoint p1, BoardPoint p2)
+		{
+			return p1.X == p2.X && p1.Y == p2.Y;
+		}
 
-            if (ReferenceEquals(p1, null) || ReferenceEquals(p2, null))
-                return false;
+		public static bool operator !=(BoardPoint p1, BoardPoint p2)
+		{
+			return !(p1 == p2);
+		}
 
-            return p1.X == p2.X && p1.Y == p2.Y;
-        }
+		public override string ToString()
+		{
+			return string.Format("[{0},{1}]", X, Y);
+		}
 
-        public static bool operator !=(BoardPoint p1, BoardPoint p2)
-        {
-            return !(p1 == p2);
-        }
+		public bool Equals(BoardPoint other)
+		{
+			return X == other.X && Y == other.Y;
+		}
 
-        public override string ToString()
-        {
-            return string.Format("[{0},{1}]", X, Y);
-        }
+		public override bool Equals(object obj)
+		{
+			return obj is BoardPoint other && Equals(other);
+		}
 
-        public override bool Equals(object obj)
-        {
-            return (BoardPoint)obj == this;
-        }
-
-        public override int GetHashCode()
-        {
-            return (X.GetHashCode() ^ Y.GetHashCode());
-        }
-    }
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (X * 397) ^ Y;
+			}
+		}
+	}
 }
